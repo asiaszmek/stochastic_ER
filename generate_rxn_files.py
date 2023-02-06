63b0877ad07313a1cf70f7fa545243a79cf3475d
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2023-02-06 15:55:17 +0100

    Files for Ca wave with RyR

---
+++ generate_rxn_files.py
@@ -0,0 +1,77 @@
+from lxml import etree
+#import argparse
+import sys
+
+file_list_full_ER = [                 
+    "Rxn_module_Ca.xml",
+    "Rxn_module_IP3R.xml",
+    "Rxn_module_mGLuR.xml",
+    "Rxn_module_RyR.xml",
+    "Rxn_module_SERCA_ER.xml",
+    "Rxn_module_SOCE.xml",
+    "Rxn_module_TG.xml"
+]
+file_tg =  "Rxn_module_TG.xml"
+file_cabuf = "Rxn_module_CaOutBuf.xml"
+file_fluo = "Rxn_module_Fluo4.xml"
+file_fura ="Rxn_module_Fura2.xml"
+
+file_list_ER_no_mGLur = [
+    "Rxn_module_Ca.xml",
+    "Rxn_module_IP3R.xml",
+    "Rxn_module_IP3.xml",
+    "Rxn_module_RyR.xml",
+    "Rxn_module_SERCA_ER.xml",
+    "Rxn_module_SOCE.xml",
+
+]
+file_list_ER_no_mGLur_no_RyR = [
+    "Rxn_module_Ca.xml",
+    "Rxn_module_IP3R.xml",
+    "Rxn_module_IP3.xml",
+    "Rxn_module_SERCA_ER.xml",
+    "Rxn_module_SOCE.xml",
+
+]
+def read_in_files(flist):
+    roots = []
+    species = set()
+    specie_diff = {}
+    rxn_ids = set()
+    my_rxn_file = etree.Element("ReactionScheme")
+
+    for fname_xml in flist:
+        print(fname_xml)
+        tree = etree.parse(fname_xml)
+        roots.append(tree.getroot())
+        for son in roots[-1]:
+            if son.tag == "Specie":
+                specie_id = son.attrib["id"]
+                specie_kdiff = son.attrib["kdiff"]
+                species.add(specie_id)
+                if specie_id in specie_diff:
+                    if specie_diff[specie_id]!= specie_kdiff:
+                        print(specie_id, specie_diff[specie_id], specie_kdiff)
+                else:
+                    specie_diff[specie_id] = specie_kdiff
+                    my_rxn_file.append(son)
+            elif son.tag == "Reaction":
+                if son.attrib["id"] in rxn_ids:
+                    print(son.attrib["id"], " already exists", fname_xml)
+                else:
+                    rxn_ids.add(son.attrib["id"])
+                    my_rxn_file.append(son)
+    return my_rxn_file
+
+if __name__ == "__main__":
+    
+    # 1 no mGluR no RyR
+    my_rxn_f = read_in_files(file_list_ER_no_mGLur_no_RyR)
+    f = open("Rxn_no_mGLuR_no_RyR.xml", "w")
+    f.write(etree.tostring(my_rxn_f, pretty_print=True).decode("utf-8"))
+    my_rxn_f = read_in_files(file_list_ER_no_mGLur)
+    f = open("Rxn_no_mGLuR.xml", "w")
+    f.write(etree.tostring(my_rxn_f, pretty_print=True).decode("utf-8"))
+    
+    
+                    
