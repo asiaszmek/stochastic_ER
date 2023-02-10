a41a962fe173eced02f36ba268f5bd8c437ac6b9
Joanna Szmek <Joanna Jedrzejewska-Szmek>
2023-02-10 14:52:13 +0100

    Dendritic sections names have changed

--- make_dend_figs.py
+++ make_dend_figs.py
@@ -77,8 +77,17 @@ def get_dynamics_in_region(my_file, specie, region, trial,
 if __name__ == '__main__':
     specie_list = ["Ca"]
     specie = "Ca"
-    reg_list = ["dend", "dend1", "dend2", "dend3", "dend4", "dend5", "dend6",
-                "dend7", "dend8", "dend9", "dend10"]
+    reg_list = ["dend", "dend01", "dend02", "dend03", "dend04", "dend05",
+                "dend06", "dend07", "dend08", "dend09", "dend10",
+                "dend11", "dend12", "dend13", "dend14", "dend15",
+                "dend16", "dend17", "dend18", "dend19", "dend20",
+                "dend21", "dend22", "dend23", "dend24", "dend25",
+                "dend26", "dend27", "dend28", "dend29", "dend30",
+                "dend31", "dend32", "dend33", "dend34", "dend35",
+                "dend36", "dend37", "dend38", "dend39", "dend40",
+                "dend41", "dend42", "dend43", "dend44", "dend45",
+                "dend46", "dend47", "dend48", "dend49", "dend50",
+                "dend51"]
     if len(sys.argv) == 1:
         sys.exit('No filename given')
     for fname in sys.argv[1:]:
