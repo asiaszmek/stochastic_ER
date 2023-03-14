ff4d3c062a6415f2d27f1f5ba1169baa22db9e08
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2023-03-14 15:55:08 +0100

    Maybe prolonged Ca closes RyRs and the wave does not propagate?
    Check short injections

--- Ca_wave_stim/change_names.py
+++ Ca_wave_stim/change_names.py
@@ -3,11 +3,11 @@ import subprocess
 
 
 file_list = os.listdir()
-string1 = "Stim_middle_3_ms"
-string2 = "Stim_middle_40_ms"
+string1 = "Stim_middle_40_ms"
+string2 = "Stim_middle_3_ms"
 
 for fname in file_list:
     if fname.startswith(string1) and fname.endswith(".xml"):
         split = fname.split(string1)
         new_fname = string2 + split[-1]
-        subprocess.run(["mv", fname, new_fname], capture_output=True)
+        subprocess.run(["cp", fname, new_fname], capture_output=True)
