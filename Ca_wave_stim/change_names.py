695234281c893ec258cee57f20758d10adc61540
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2023-03-14 15:45:46 +0100

    There will be a shorter injection

---
+++ Ca_wave_stim/change_names.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+
+file_list = os.listdir()
+string1 = "Stim_middle_3_ms"
+string2 = "Stim_middle_40_ms"
+
+for fname in file_list:
+    if fname.startswith(string1) and fname.endswith(".xml"):
+        split = fname.split(string1)
+        new_fname = string2 + split[-1]
+        subprocess.run(["mv", fname, new_fname], capture_output=True)
