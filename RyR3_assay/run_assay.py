a886e8b632eac2fd1609cd67350f1e2b84b479c1
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2023-03-21 11:25:51 +0100

    Separate files for the Keizer and Levine cases

--- RyR3_assay/run_assay.py
+++ RyR3_assay/run_assay.py
@@ -1,14 +1,152 @@
 import sys
 import subprocess
+from datetime import date
 from lxml import etree
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-KL_list = ["RyR", "RyRC1",
-           "RyRC2", "RyRC3",
-           "RyRC4", "RyRC5",
-           "RyRI"]
+ca_conc_file = "po_pCa.csv"
+ryr_op_fname = "open_duration_pCa.csv"
+ryr_cl_fname = "closed_duration_pCa.csv"
+
+model_text = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<SDRun xmlns:xi="http://www.w3.org/2001/XInclude" xmlns="http://stochdiff.textensor.org">
+    <xi:include href="../%s" />
+    <xi:include href="Morph.xml" />
+    <xi:include href="%s" />
+    <xi:include href="IO_Ca.xml"/>
+    <!--2D means the morphology is interpreted like a flatworm, 3D for
+roundworms. The 2D case is good for testing as it is easy to visualize the
+results (also, 3D may not work yet...)  -->
+   
+    <geometry>          2D           </geometry>
+    <depth2D>           10          </depth2D>
+    <distribution>      BINOMIAL     </distribution>
+    <algorithm>         INDEPENDENT  </algorithm>
+    <simulationSeed>    245         </simulationSeed>
+    <outputQuantity>NUMBER</outputQuantity>
+
+    <!-- run time for the calculation, milliseconds -->
+    <runtime>143000</runtime>
+
+    <!-- set the seed to get the same spines each time testing -->
+    <spineSeed>123</spineSeed>
+
+    <discretization>
+      <defaultMaxElementSide>10</defaultMaxElementSide>
+      <surfaceLayers>10</surfaceLayers> 
+    </discretization>
+    <tolerance>0.01</tolerance>
+
+    <outputInterval>500</outputInterval>
+
+    <calculation>GRID_ADAPTIVE</calculation>
+
+</SDRun>"""
+
+
+
+IC_text = {
+    "KL": """<?xml version="1.0" encoding="utf-8"?>
+<InitialConditions>
+  <ConcentrationSet>
+    <NanoMolarity specieID="Ca" value="%f"/>
+    <NanoMolarity specieID="RyRC1"      value="0.1"    />
+  </ConcentrationSet>
+</InitialConditions>
+""",
+ "KLtuned": """<?xml version="1.0" encoding="utf-8"?>
+<InitialConditions>
+  <ConcentrationSet>
+    <NanoMolarity specieID="Ca" value="%f"/>
+    <NanoMolarity specieID="RyRC1"      value="0.1"    />
+  </ConcentrationSet>
+</InitialConditions>
+""",
+    "Saftenku": """<?xml version="1.0" encoding="utf-8"?>
+<InitialConditions>
+  <ConcentrationSet>
+    <NanoMolarity specieID="Ca" value="%f"/>
+    <NanoMolarity specieID="RyR1L"      value="0.1"    />
+  </ConcentrationSet>
+</InitialConditions>
+""",
+    "Dura": 
+    """<?xml version="1.0" encoding="utf-8"?>
+    <InitialConditions>
+    <ConcentrationSet>
+    <NanoMolarity specieID="Ca" value="%f"/>
+    <NanoMolarity specieID="RyR"      value="0.1"    />
+    </ConcentrationSet>
+    </InitialConditions>
+    """
+}
+IO_text ={
+    "KL":
+    """<OutputScheme>
+  <OutputSet filename = "all"  dt=".1">
+    <OutputSpecie name="Ca"/>
+    <OutputSpecie name="RyRO1"/>
+    <OutputSpecie name="RyRO2"/>
+    <OutputSpecie name="RyRC1"/>
+    <OutputSpecie name="RyRC2"/>
+  </OutputSet>
+</OutputScheme>""",
+     "KLtuned":
+    """<OutputScheme>
+  <OutputSet filename = "all"  dt=".1">
+    <OutputSpecie name="Ca"/>
+    <OutputSpecie name="RyRO1"/>
+    <OutputSpecie name="RyRO2"/>
+    <OutputSpecie name="RyRC1"/>
+    <OutputSpecie name="RyRC2"/>
+  </OutputSet>
+</OutputScheme>""",
+    "Dura":
+    """<OutputScheme>
+  <OutputSet filename = "all"  dt=".1">
+    <OutputSpecie name="Ca"/>
+    <OutputSpecie name="RyRO1"/>
+    <OutputSpecie name="RyRO2"/>
+    <OutputSpecie name="RyRC1"/>
+    <OutputSpecie name="RyRC2"/>
+    <OutputSpecie name="RyRC4"/>
+    <OutputSpecie name="RyRC5"/>
+    <OutputSpecie name="RyR"/>
+    <OutputSpecie name="RyRI"/>
+  </OutputSet>
+</OutputScheme>""",
+
+    "Saftenku": """<OutputScheme>
+    <OutputSet filename = "all"  dt=".1">
+    <OutputSpecie name="Ca"/>
+    <OutputSpecie name="RyR1L"/>
+    <OutputSpecie name="RyR2L"/>
+    <OutputSpecie name="RyR3L"/>
+    <OutputSpecie name="RyR4L"/>
+    <OutputSpecie name="RyR5L"/>
+    <OutputSpecie name="RyRLO1"/>
+    <OutputSpecie name="RyRLO2"/>
+    <OutputSpecie name="RyRLO3"/>
+    <OutputSpecie name="RyR1H"/>
+    <OutputSpecie name="RyR2H"/>
+    <OutputSpecie name="RyR3H"/>
+    <OutputSpecie name="RyR4H"/>
+    <OutputSpecie name="RyRHO1"/>
+    <OutputSpecie name="RyRHO2"/>
+  </OutputSet>
+</OutputScheme>"""
+}
+
+Rxn_file = {
+    "KL": "Rxn_module_RyR_KeizerLevine.xml",
+    "Dura": "Rxn_module_RyR_Dura.xml",
+    "Saftenku": "Rxn_module_RyR_Saftenku.xml",
+    "KLtuned": "Rxn_module_RyR_KLtuned.xml",
+}
+
+
 
 def get_key(cell):
     if cell[18]:
@@ -63,18 +201,40 @@ def sum_volume(my_file, region_list):
             vol_sum += volumes[region]
     return vol_sum
 
-def get_all_closed(data, species, specie_list):
+def get_all_closed(data, species):
     count = 0
     sum_times = 0
     out = []
-    for specie in specie_list:
+        
+    for specie in species:
+        if "O" in specie:
+            continue
+        if "RyR" not in specie:
+            continue
         specie_state = data[:, 0, species.index(specie)]
-        if "RyR" not in specie_list:
-            count += len(np.where((specie_state[1:] - specie_state[0:-1])==1)[0])
-    
+        count += len(np.where((specie_state[1:] - specie_state[0:-1])==1)[0])
         sum_times += specie_state.sum()
         out.append(specie_state[-1])
     return sum_times, count, out
+
+
+def get_all_open(data, species):
+    count = 0
+    sum_times = 0
+    out = []
+        
+    for specie in species:
+        if "O" not in specie:
+            continue
+        if "RyR" not in specie:
+            continue
+
+        specie_state = data[:, 0, species.index(specie)]
+        count += len(np.where((specie_state[1:] - specie_state[0:-1])==1)[0])
+        sum_times += specie_state.sum()
+        out.append(specie_state[-1])
+    return sum_times, count, out
+
 
 def get_numbers(my_file, output="all"):
     output_dict = get_output_regions(my_file)
@@ -82,6 +242,11 @@ def get_numbers(my_file, output="all"):
     open_ryr3 = []
     mean_c_t = []
     mean_o_t = []
+    no = 0
+    sum_o = 0
+    nc = 0
+    sum_c = 0
+ 
     for trial in my_file.keys():
         if trial == "model":
             continue
@@ -92,8 +257,18 @@ def get_numbers(my_file, output="all"):
         dt = times[1]-times[0]
         exp_len = int((times[-1])/dt)
         mean_ca = data[:, 0, species.index("Ca")].mean()*10/6.023/vol
-        ryr_basal = data[0, 0, species.index("RyRC1")]
-        open_sum, tot_no, ends = get_all_closed(data, species, ["RyRO1", "RyRO2"])
+        
+        if "RyR" in species:
+            bas_idx = species.index("RyR")
+        elif "RyRC1" in species:
+            bas_idx = species.index("RyRC1")
+        elif "RyR1L" in species:
+            bas_idx = species.index("RyR1L")
+        
+        ryr_basal = data[0, 0, bas_idx]
+        
+        
+        open_sum, tot_no, ends = get_all_open(data, species)
         if 1 in ends:
             end_closed = False
         else:
@@ -101,173 +276,108 @@ def get_numbers(my_file, output="all"):
         p_open_ryr = open_sum/ryr_basal/exp_len
         Ca_conc.append(mean_ca)
         open_ryr3.append(p_open_ryr)
-
         if ryr_basal != 1:
             continue
         if tot_no > 0:
-            mean_o_t.append(dt*open_sum/tot_no)     
+            no += tot_no
+            sum_o += open_sum
         
-        sum_closed, tot_nc, ends  = get_all_closed(data, species, ["RyRC1", "RyRC2"] )
+        sum_closed, tot_nc, ends  = get_all_closed(data, species)
 
         if end_closed:
             tot_nc += 1
         tot_nc += tot_no
         if tot_nc > 0:
-            mean_c_t.append(dt*sum_closed/tot_nc)
+            sum_c +=sum_closed
+            nc += tot_nc
+    if  nc != 0:
+       mean_c_t = dt*sum_c/nc
+    else:
+        mean_c_t = 0
+    if no != 0: 
+        mean_o_t = dt*sum_o/no
+    else:
+        mean_o_t = 0
+        mean_c_t = 0
     return Ca_conc, open_ryr3, mean_o_t, mean_c_t
 
 
 
         
-model_text = """<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<SDRun xmlns:xi="http://www.w3.org/2001/XInclude" xmlns="http://stochdiff.textensor.org">
-    <xi:include href="../Rxn_module_RyR_KeizerLevine.xml" />
-    <xi:include href="Morph.xml" />
-    <xi:include href="%s" />
-    <xi:include href="IO_Ca.xml"/>
-    <!--2D means the morphology is interpreted like a flatworm, 3D for
-roundworms. The 2D case is good for testing as it is easy to visualize the
-results (also, 3D may not work yet...)  -->
-   
-    <geometry>          2D           </geometry>
-    <depth2D>           10          </depth2D>
-    <distribution>      BINOMIAL     </distribution>
-    <algorithm>         INDEPENDENT  </algorithm>
-    <simulationSeed>    245         </simulationSeed>
-    <outputQuantity>NUMBER</outputQuantity>
 
-    <!-- run time for the calculation, milliseconds -->
-    <runtime>143000</runtime>
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        model = "KLtuned"
+    else:
+        model = sys.argv[1]
+        
+    f = open("IO_Ca.xml", "w")
+    f.write(IO_text[model])
+    f.close()
+    exp_res = np.loadtxt(ca_conc_file, skiprows=1, delimiter=',')
+    ca_conc_list = exp_res[:, 0]
+    output = np.zeros(exp_res.shape)
+    mean_times = []
+    for i, ca_conc in enumerate(ca_conc_list):
+        ca_conc_nM = int(np.ceil(ca_conc*1e9))
+        IC_name = "Ca_%d.xml" % ca_conc_nM
+        model_name = "RyR_model_Ca_%d.xml" % ca_conc_nM
+        output_name = "RyR_model_Ca_%d.h5" % ca_conc_nM
+        fic = open(IC_name, "w")
+        fic.write(IC_text[model] % ca_conc_nM)
+        fic.close()
+        fm = open(model_name, "w")
+        fm.write(model_text % (Rxn_file[model], IC_name))
+        fm.close()
+        process = subprocess.run(["/usr/lib/jvm/java-8-openjdk-amd64/bin/java",
+                                  "-jar",
+                                  "/home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar",
+                                  "-Dneurord.trials=10", model_name],
+                                 capture_output=True)
 
-    <!-- set the seed to get the same spines each time testing -->
-    <spineSeed>123</spineSeed>
-
-    <discretization>
-      <defaultMaxElementSide>10</defaultMaxElementSide>
-      <surfaceLayers>10</surfaceLayers> 
- 
-      
-
-    </discretization>
-
-
-    <!-- the tolerace is not used yet -->
-    <tolerance>0.001</tolerance>
-
-    <outputInterval>500</outputInterval>
-
-
-
-    <calculation>GRID_ADAPTIVE</calculation>
-
-</SDRun>"""
-
-
-
-IC_text_KL = """<?xml version="1.0" encoding="utf-8"?>
-<InitialConditions>
-  <ConcentrationSet>
-    <NanoMolarity specieID="Ca" value="%f"/>
-    <NanoMolarity specieID="RyRC1"      value="0.1"    />
-  </ConcentrationSet>
-</InitialConditions>
-"""
-
-IO_KL = """
-<OutputScheme>
-  <OutputSet filename = "all"  dt=".1">
-    <OutputSpecie name="Ca"/>
-    <OutputSpecie name="RyRO1"/>
-    <OutputSpecie name="RyRO2"/>
-    <OutputSpecie name="RyRC1"/>
-    <OutputSpecie name="RyRC2"/>
-  </OutputSet>
-
-</OutputScheme>"""
-
-
-IO_Dura = """
-<OutputScheme>
-  <OutputSet filename = "all"  dt=".1">
-    <OutputSpecie name="Ca"/>
-    <OutputSpecie name="RyRO1"/>
-    <OutputSpecie name="RyRO2"/>
-    <OutputSpecie name="RyRC1"/>
-    <OutputSpecie name="RyRC2"/>
-    <OutputSpecie name="RyRC4"/>
-    <OutputSpecie name="RyRC5"/>
-    <OutputSpecie name="RyR"/>
-    <OutputSpecie name="RyRI"/>
-
-  </OutputSet>
-
-</OutputScheme>"""
-
-
-f = open("IO_Ca.xml", "w")
-f.write(IO_KL)
-f.close()
-ca_conc_file = "po_pCa.csv"
-ryr_op_fname = "open_duration_pCa.csv"
-ryr_cl_fname = "closed_duration_pCa.csv"
-exp_res = np.loadtxt(ca_conc_file, skiprows=1, delimiter=',')
-ca_conc_list = exp_res[:, 0]
-output = np.zeros(exp_res.shape)
-mean_times = []
-for i, ca_conc in enumerate(ca_conc_list):
-
-    ca_conc_nM = int(np.ceil(ca_conc*1e9))
-    IC_name = "Ca_%d.xml" % ca_conc_nM
-    model_name = "RyR_model_Ca_%d.xml" % ca_conc_nM
-    output_name = "RyR_model_Ca_%d.h5" % ca_conc_nM
-    fic = open(IC_name, "w")
-    fic.write(IC_text_KL % ca_conc_nM)
-    fic.close()
-    fm = open(model_name, "w")
-    fm.write(model_text % IC_name)
-    fm.close()
-    process = subprocess.run(["/usr/lib/jvm/java-8-openjdk-amd64/bin/java",
-                              "-jar",
-                              "/home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar",
-                              "-Dneurord.trials=10", model_name],
-                             capture_output=True)
-    if not process.returncode:
-        my_file = h5py.File(output_name, 'r')
-        conc, po, t_o, t_c = get_numbers(my_file, output="all")
-        output[i, 0] = np.mean(conc)
-        output[i, 1] = np.mean(po)
-        if len(t_o) == 0 or len(t_c) == 0:
-            continue
-        mean_times.append([np.mean(conc), np.mean(t_o), np.mean(t_c)])
+        if not process.returncode:
+            my_file = h5py.File(output_name, 'r')
+            conc, po, t_o, t_c = get_numbers(my_file, output="all")
+            output[i, 0] = np.mean(conc)
+            output[i, 1] = np.mean(po)
+            print(output[i])
+            mean_times.append([np.mean(conc), t_o, t_c])
+            print(t_o, t_c)
+            
 
             
-exp_open = np.loadtxt(ryr_op_fname, skiprows=1, delimiter=",")
-exp_closed = np.loadtxt(ryr_cl_fname, skiprows=1, delimiter=",")
-mean_times_a = np.array(mean_times)
-
-fig, ax = plt.subplots(1)
-ax.set_xscale('log')
-ax.plot(exp_res[:, 0], exp_res[:, 1], "d", color="tab:blue", label="experimental data")
-ax.plot(output[:, 0]*1e-9, output[:, 1], "d", color="tab:red", label="model data")
-ax.legend()
-ax.set_xlabel("Concentration [M]")
-ax.set_ylabel("RyR3 open probability")
-
-fig, ax = plt.subplots(1)
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.plot(exp_open[:, 0], exp_open[:, 1], "d", color="tab:blue",
-        label="exp open")
-ax.plot(exp_closed[:, 0], exp_closed[:, 1], "d", color="tab:green",
-        label="exp closed")
-ax.plot(mean_times_a[:, 0]*1e-9, mean_times_a[:, 1], "d",
-        label="model open", color="tab:cyan")
-ax.plot(mean_times_a[:, 0]*1e-9, mean_times_a[:, 2], "d",
-        label="exp closed", color="tab:olive")
-
-ax.legend()
-ax.set_xlabel("Concentration [M]")
-ax.set_ylabel("Time [ms]")
+    exp_open = np.loadtxt(ryr_op_fname, skiprows=1, delimiter=",")
+    exp_closed = np.loadtxt(ryr_cl_fname, skiprows=1, delimiter=",")
+    mean_times_a = np.array(mean_times)
+    date = date.today().strftime("%y%m%d")
+    res_fname1 = "po_res_%s_%s.csv" % (model, date)
+    res_fname2 = "open_closed_times_res_%s_%s.csv" % (model, date)
+    np.savetxt(res_fname1, output, delimiter=",", header="Ca [nM], po")
+    np.savetxt(res_fname2, mean_times_a, delimiter=",",
+               header="Ca [nM], mean open time, mean closed time")
+    fig, ax = plt.subplots(1)
+    ax.set_xscale('log')
+    ax.plot(exp_res[:, 0], exp_res[:, 1], "d", color="tab:blue", label="experimental data")
+    ax.plot(output[:, 0]*1e-9, output[:, 1], "d", color="tab:red", label="model data")
+    ax.legend()
+    ax.set_xlabel("Concentration [M]")
+    ax.set_ylabel("RyR3 open probability")
+    
+    fig, ax = plt.subplots(1)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.plot(exp_open[:, 0], exp_open[:, 1], "d", color="tab:blue",
+            label="exp open")
+    ax.plot(exp_closed[:, 0], exp_closed[:, 1], "d", color="tab:green",
+            label="exp closed")
+    ax.plot(mean_times_a[:, 0]*1e-9, mean_times_a[:, 1], "d",
+            label="model open", color="tab:cyan")
+    ax.plot(mean_times_a[:, 0]*1e-9, mean_times_a[:, 2], "d",
+            label="exp closed", color="tab:olive")
+    
+    ax.legend()
+    ax.set_xlabel("Concentration [M]")
+    ax.set_ylabel("Time [ms]")
 
 
 plt.show()
