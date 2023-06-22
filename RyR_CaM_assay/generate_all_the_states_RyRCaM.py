4771bfbf56a48259f68166b35338bd8885ef0e7f
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2023-06-22 11:29:45 +0200

    Write 2 different CaM units reactions

--- RyR_CaM_assay/generate_all_the_states_RyRCaM.py
+++ RyR_CaM_assay/generate_all_the_states_RyRCaM.py
@@ -20,8 +20,27 @@ kr_rev = {"2N": 35e-4, "2C": 60e-3}
 def make_product_name_2(no_specie1, specie1, no_specie2, specie2, RyR_states):
     if no_specie1 == 4 and no_specie2 == 0:
         return "RyR_4%s" % specie1
+    if no_specie1 == 0 and no_specie2 == 4:
+        return "RyR_4%s" % specie2
     new_name = "RyR_%d%s_%d%s" % (no_specie1, specie1, no_specie2, specie2)
     return check_specie_2(new_name, RyR_states)
+
+
+def make_product_name_3(no_specie1, specie1, no_specie2, specie2, no_specie3,
+                        specie3, RyR_states):
+    if no_specie1 == 0:
+        new_name = "RyR_%d%s_%d%s" % (no_specie2, specie2, no_specie3, specie3)
+        return check_specie_2(new_name, RyR_states)
+    if no_specie2 == 0:
+        new_name = "RyR_%d%s_%d%s" % (no_specie1, specie1, no_specie3, specie3)
+        return check_specie_2(new_name, RyR_states)
+    if no_specie3 == 0:
+        new_name = "RyR_%d%s_%d%s" % (no_specie1, specie1, no_specie2, specie2)
+        return check_specie_2(new_name, RyR_states)
+    
+    new_name = "RyR_%d%s_%d%s_%d%s" % (no_specie1, specie1, no_specie2,
+                                      specie2, no_specie3, specie3)
+    return check_specie_3(new_name, RyR_states)
 
 
 def add_reaction(root1, specie1, product1, kf1, kr1, rxn_idx):
@@ -35,8 +54,8 @@ def add_reaction(root1, specie1, product1, kf1, kr1, rxn_idx):
     kf.text = str(kf1)
     kr = etree.SubElement(my_reac, "reverseRate")
     kr.text = str(kr1)
-    print(etree.tostring(my_reac))
     return my_reac
+
 
 def check_specie_2(specie, RyR_states):
     if specie in RyR_states:
@@ -44,19 +63,27 @@ def check_specie_2(specie, RyR_states):
     splicik = specie.split("_")
     if len(splicik) == 3:
         return "RyR_%s_%s" % (splicik[2], splicik[1])
-    return
+
 
 def check_specie_3(specie, RyR_states):
     if specie in RyR_states:
         return specie
     splicik = specie.split("_")
-    alt = "RyR_%s_%s_%s" %( splicik[2], splicik[0], splicik[1])
+    alt = "RyR_%s_%s_%s" %( splicik[3], splicik[1], splicik[2])
     if alt in RyR_states:
         return alt
-    alt = "RyR_%s_%s_%s" %( splicik[2], splicik[1], splicik[0])
+    alt = "RyR_%s_%s_%s" %( splicik[3], splicik[2], splicik[1])
     if alt in RyR_states:
         return alt
-    return
+    alt = "RyR_%s_%s_%s" %( splicik[1], splicik[3], splicik[2])
+    if alt in RyR_states:
+        return alt
+    alt = "RyR_%s_%s_%s" %( splicik[2], splicik[1], splicik[3])
+    if alt in RyR_states:
+        return alt
+    alt = "RyR_%s_%s_%s" %( splicik[2], splicik[3], splicik[1])
+    if alt in RyR_states:
+        return alt
 
     
 #RyR binding calmodulin
@@ -116,7 +143,6 @@ rxn_idx = 0
 
 for ryr_specie in RyR_states:
     components = ryr_specie.split("_")
-
     if len(components) == 2:
         if ryr_specie == "RyR_4CaMCa4":
             continue
@@ -151,19 +177,68 @@ for ryr_specie in RyR_states:
             camca4_idx = None
 
         if camca4_idx is not None:
-            print(components, no_s, species, no_s[camca4_idx], no_s[not camca4_idx])
             no_camca4 = no_s[camca4_idx]
             specie1 = species[not camca4_idx]
             no_specie = no_s[not camca4_idx]
             endswith = specie1[-2:]
-            print(endswith)
             #specie binding reactions:
-            print(no_camca4, no_specie, ryr_specie)
             if endswith in ["2C", "2N"]:
                 product = make_product_name_2(no_camca4+1, "CaMCa4", no_specie-1, specie1, RyR_states)
                 add_reaction(root, ryr_specie, product, kf_rev[endswith], kr_rev[endswith], rxn_idx)
                 rxn_idx += 1
+            else:
+                product = make_product_name_3(no_camca4, "CaMCa4", no_specie-1, specie1,
+                                              1, "CaMCa2N", RyR_states)
+                add_reaction(root, ryr_specie, product, kf["2N"], kr["2N"], rxn_idx)
+                rxn_idx += 1
+                product = make_product_name_3(no_camca4, "CaMCa4", no_specie-1,
+                                              specie1, 1, "CaMCa2C", RyR_states)
+                add_reaction(root, ryr_specie, product, kf["2C"], kr["2C"], rxn_idx)
+                rxn_idx += 1
+            continue
+
+        if "CaM" in species:
+            cam_idx = species.index("CaM")
+        else:
+            cam_idx = None
+
+        if cam_idx is not None:
+            no_cam = no_s[cam_idx]
+            specie1 = species[not cam_idx]
+            no_specie = no_s[not cam_idx]
+            endswith = specie1[-2:]
+            if endswith in ["2C", "2N"]:
+                product = make_product_name_3(1, "CaMCa4", no_cam, "CaM", no_specie-1,
+                                              specie1, RyR_states)
+                print(ryr_specie, product)
+                print(1, "CaMCa4", no_cam, "CaM", no_specie-1,
+                      specie1)
+                
+                add_reaction(root, ryr_specie, product, kf_rev[endswith], kr_rev[endswith], rxn_idx)
+                rxn_idx += 1
+                product = make_product_name_2(no_cam-1, 'CaM', no_specie+1, specie1, RyR_states)
+                add_reaction(root, ryr_specie, product, kf[endswith], kr[endswith], rxn_idx)
+                rxn_idx += 1
+            continue
+        #   RyR_n1CaMCa2C_n2CaMCa2N
+        specie1 = species[0]
+        no_1 = no_s[0]
+        specie2 = species[1]
+        no_2 = no_s[1]
+
+        #  1: RyR_n1CaMCa2C_n2CaMCa2N + 2 Ca <-> RyR_(n1-1)CaMCa2C_n2CaMCa2N_1CaMCa4 kf["2N"], kr["2N"]
+        product = make_product_name_3(1, "CaMCa4", no_1-1, specie1, no_2,
+                                      specie2, RyR_states)
+        print(1, "CaMCa4", no_1-1, specie1, no_2, specie2, ryr_specie, product)
+        add_reaction(root, ryr_specie, product, kf_rev[specie1[-2:]], kr_rev[specie1[-2:]], rxn_idx)
+        rxn_idx += 1
+        #  2: RyR_n1CaMCa2C_n2CaMCa2N + 2 Ca <-> RyR_n1CaMCa2C_(n2-1)CaMCa2N_1CaMCa4 kf["2C"], kr["2C"]
+        product = make_product_name_3(1, "CaMCa4", no_1, specie1, no_2-1,
+                                      specie2, RyR_states)
+        add_reaction(root, ryr_specie, product, kf_rev[specie2[-2:]], kr_rev[specie2[-2:]], rxn_idx)
+        rxn_idx += 1
         
+                
                 
 my_file.write('<?xml version="1.0"?>\n')
 my_file.write(etree.tostring(root, pretty_print=True).decode('utf-8'))
