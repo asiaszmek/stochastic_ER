dcf146065df37e3f3fd168e06e54b1672638a305
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2023-10-09 09:33:16 +0200

    Correct
    Signed-off-by: Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>

--- Ca_wave_RyR2CaM_simple_SERCA_SOCE/bash_short_stim_1.2.sh
+++ Ca_wave_RyR2CaM_simple_SERCA_SOCE/bash_short_stim_1.2.sh
@@ -1,6 +1,6 @@
-nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_0350_nM.xml&
-nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_0700_nM.xml&
-nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_1050_nM.xml&
-nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_1050_nM.xmlFura2.xml&
-nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_0700_nM.xmlFura2.xml&
-nohup /usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_0350_nM.xmlFura2.xml&
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10 model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_0350_nM.xml
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10 model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_0700_nM.xml
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10 model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_1050_nM.xml
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10 model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_1050_nM.xmlFura2.xml
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10 model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_0700_nM.xmlFura2.xml
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar /home/jszmek/new_neurord/neurord-3.2.3-all-deps.jar -Dneurord.trials=10 model_RyR2CaM_3s_injection_simple_SERCA_SOCE_tubes_diam_1.2_um_50_um_0350_nM.xmlFura2.xml
