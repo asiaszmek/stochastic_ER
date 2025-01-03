e2eb72a13563e3f705b200a94a5ed86f7ccf2308
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2025-01-03 14:58:06 +0100

    Generate Ca peaks figures for cases with Ca oscillations/sparks

--- scripts/run_basal_fig.py
+++ scripts/run_basal_fig.py
@@ -5,12 +5,13 @@ import numpy as np
 from scipy.fft import fft, fftfreq, fftshift
 import matplotlib.pyplot as plt
 import utility_functions as ut
+
+
 colors =  {
     "1.2": 'tab:blue',
     "2.4": 'tab:purple',
     "6.0": 'tab:green'
 }
-
 names_dict = {
     "ctrl" :
     "model_RyRCaM_simple_SERCA_SOCE_tubes_diam_%s_um_10_um_dendrite.h5",
@@ -28,9 +29,10 @@ names_dict = {
     "model_2x_RyR_RyRCaM_0.8_PMCA_simple_SERCA_tubes_diam_%s_um_2_um_dendrite.h5"
 }
 
+
 dend_diam = ["1.2", "2.4", "6.0"]
-#trial = "trial0"
 output = "__main__"
+
 
 def adjust_axes(ax):
     mini = min([min(x.get_xlim()) for x in ax])
@@ -40,109 +42,42 @@ def adjust_axes(ax):
         x.set_xlim([mini, maxi])
 
 
-def find_peaks(array):
-    peak_time = []
-    peak_width = []
-    peak_amp = []
-    if len(np.where(array > 150)[0]):
-        peaks = np.where(array > 110)[0]
-        if len(peaks):
-            ranges = np.where(peaks[1:]-peaks[:-1] > 50)[0]
-            idx_pre = peaks[0]
-            for r in ranges:
-                idx_post = peaks[r]
-                idx = np.argmax(array[idx_pre:idx_post])+idx_pre
-                peak_amp.append(array[idx])
-                peak_time.append(idx*200)
-                peak_width.append((idx_post-idx_pre)*200)
-                idx_pre = idx_post
-            if array[idx_pre:peaks[-1]].max() > 110:
-                idx = np.argmax(array[idx_pre:peaks[-1]])+idx_pre
-                peak_time.append(idx*200)
-                peak_width.append((peaks[-1]-idx_pre)*200)
-                peak_amp.append(array[idx])
-    return peak_time, peak_width, peak_amp
-
 if __name__ == "__main__":
     data_dir = os.path.join("..", "stacked_ER")
     x_labels = []
     x_labels_pmca = []
     fig_m_ca, ax_m_ca = plt.subplots(1, len(dend_diam),
                                            figsize=(len(dend_diam)*5, 5))
-    fig_peak_no, ax_peak_no = plt.subplots(1, len(dend_diam),
-                                           figsize=(len(dend_diam)*5, 5))
-    fig_m_peak_amp, ax_m_peak_amp = plt.subplots(1, len(dend_diam),
-                                                 figsize=(len(dend_diam)*5, 5))
-    fig_m_peak_width, ax_m_peak_width = plt.subplots(1, len(dend_diam),
-                                                     figsize=(len(dend_diam)*5,
-                                                              5)) 
-    fig_m_peak_dist, ax_m_peak_dist = plt.subplots(1, len(dend_diam),
-                                                   figsize=(len(dend_diam)*5,
-                                                            5))
-   
-    
+
     for i, d in enumerate(dend_diam):
         means = []
         stds = []
-        no_mean = []
-        no_std = []
-        amp_mean = []
-        amp_std = []
-        width_mean = []
-        width_std = []
-        dist_mean = []
-        dist_std = []
         x_labels = []
-        x_labels_pmca = []
+        
         for key, fname in names_dict.items():
             path = os.path.join(data_dir, fname % d)
             my_file = h5py.File(path)
             grid_list = ut.get_grid_list(my_file)
             conc_list = []
             conc_std = []
-            peak_no = []
-            peak_amp = []
-            peak_width = []
-            peak_dist = []
             for trial in ["trial0", "trial1", "trial2", "trial3"]:
-                data = ut.get_populations(my_file, trial=trial, output=output)
+                data = ut.get_populations(my_file, trial=trial,
+                                          output=output)
                 specie_idx = ut.get_all_species(my_file,
                                                 output=output).index("Ca")
                 volume = sum(list(ut.region_volumes(my_file).values()))
                 
-                tot_conc =  ut.nano_molarity(data[:, :, specie_idx].sum(axis=1),
+                tot_conc =  ut.nano_molarity(data[:, :,
+                                                  specie_idx].sum(axis=1),
                                          volume)
-                
-                peaks, peak_w, peak_amp_i = find_peaks(tot_conc)
-
-                if len(peaks):
-                    peak_no.append(len(peaks))
-                    peak_width += peak_w
-                    peak_amp += peak_amp_i
-                    peak_dist += [d-peaks[i-1] for i, d in enumerate(peaks[1:])]
-                else:
-                    peak_no.append(0)
-                    peak_amp.append(0)
-                    peak_width.append(0)
-                    peak_dist.append(0)
                 conc_list.append(tot_conc.mean())
                 conc_std.append(tot_conc.std())
 
             means.append(np.mean(conc_list))
             stds.append(sum([s**2 for s in conc_std])**0.5/2)
             x_labels.append(key)
-            if "low PMCA" in key:
-                print(key)
-                x_labels_pmca.append(key)
-                no_mean.append(np.mean(peak_no))
-                no_std.append(np.std(peak_no)/len(peak_no)**0.5)
-                amp_mean.append(np.mean(peak_amp))
-                amp_std.append(np.std(peak_amp)/len(peak_amp)**.5)
-                width_mean.append(np.mean(peak_width)/1000)
-                width_std.append(np.std(peak_width)/len(peak_width)**.5/1000)
-                dist_mean.append(np.mean(peak_dist)/1000)
-                dist_std.append(np.std(peak_dist)/len(peak_dist)**.5/1000)
-        print(d, no_mean,  amp_mean, width_mean, dist_mean)
+    
+ 
         ax_m_ca[i].errorbar(y=x_labels, x=means, xerr=stds,
                         color=colors[d],
                         marker="o", linestyle="")
@@ -150,63 +85,7 @@ if __name__ == "__main__":
         ax_m_ca[i].set_yticklabels([])
         ax_m_ca[i].set_xlabel("mean Ca (nM)")
         
-        ax_peak_no[i].errorbar(y=x_labels_pmca, x=no_mean,
-                               xerr=no_std,
-                               color=colors[d],
-                               marker="o", linestyle="")
-        ax_peak_no[i].set_title("diam %s um" % d)
-        ax_peak_no[i].set_yticklabels([])
-        ax_peak_no[i].set_xlabel("# calcium peaks")
-        ax_m_peak_amp[i].errorbar(y=x_labels_pmca, x=amp_mean,
-                                  xerr=amp_std,
-                                  color=colors[d],
-                                  marker="o", linestyle="")
-        
-        ax_m_peak_amp[i].set_yticklabels([])
-        ax_m_peak_amp[i].set_title("diam %s um" % d)
-        ax_m_peak_amp[i].set_xlabel("mean Ca peak amplitude (nM)")
-        ax_m_peak_width[i].errorbar(y=x_labels_pmca, x=width_mean,
-                                    xerr=width_std,
-                                    color=colors[d],
-                                    marker="o", linestyle="")
-        
-        ax_m_peak_width[i].set_yticklabels([])
-        ax_m_peak_width[i].set_title("diam %s um" % d)
-        ax_m_peak_width[i].set_xlabel("mean Ca peak width (s)")
-
-        ax_m_peak_dist[i].errorbar(y=x_labels_pmca, x=dist_mean,
-                                   xerr=dist_std,
-                                   color=colors[d],
-                                   marker="o", linestyle="")
-        
-        ax_m_peak_dist[i].set_yticklabels([])
-        ax_m_peak_dist[i].set_title("diam %s um" % d)
-        ax_m_peak_dist[i].set_xlabel("mean interval between peaks (s)")
-
-        
     ax_m_ca[0].set_yticklabels(x_labels)
-    ax_peak_no[0].set_yticklabels(x_labels_pmca)
-    ax_m_peak_amp[0].set_yticklabels(x_labels_pmca)
-    ax_m_peak_width[0].set_yticklabels(x_labels_pmca)
-    ax_m_peak_dist[0].set_yticklabels(x_labels_pmca)
-    ax_peak_no[0].set_yticklabels(x_labels_pmca)
-    ax_m_peak_amp[0].set_yticklabels(x_labels_pmca)
-    ax_m_peak_width[0].set_yticklabels(x_labels_pmca)
-    ax_m_peak_dist[0].set_yticklabels(x_labels_pmca)
     adjust_axes(ax_m_ca)
-    adjust_axes(ax_peak_no)
-    adjust_axes(ax_m_peak_amp)
-    adjust_axes(ax_m_peak_width)
-    adjust_axes(ax_m_peak_dist)
     fig_m_ca.savefig("mean_basal_ca.png", dpi=100,
                  bbox_inches="tight")
-    fig_peak_no.savefig("mean_peak_no.png", dpi=100,
-                 bbox_inches="tight")
-    fig_m_peak_amp.savefig("mean_peak_amp.png", dpi=100,
-                 bbox_inches="tight")
-    fig_m_peak_dist.savefig("mean_peak_dist.png", dpi=100,
-                            bbox_inches="tight")
-    fig_m_peak_width.savefig("mean_peak_width.png", dpi=100,
-                            bbox_inches="tight")
-
-    plt.show()
--- scripts/run_edges_fig.py
+++ scripts/run_edges_fig.py
@@ -26,18 +26,10 @@ names_dict = {
 dend_diam = ["1.2", "2.4", "6.0"]
 output = "__main__"
 
-def adjust_axes(ax):
-    mini = min([min(x.get_xlim()) for x in ax])
-    maxi = max([max(x.get_xlim()) for x in ax])
-    for x in ax:
-      
-        x.set_xlim([mini, maxi])
-
         
 def find_coords(P0, P1):
     return [[min(P0[0], P1[0]), min(P0[1], P1[1])],
             [max(P0[0], P1[0]), max(P0[1], P1[1])]]
-
 
 
 def is_y(miny, maxy, old_miny, old_maxy):
@@ -45,23 +37,126 @@ def is_y(miny, maxy, old_miny, old_maxy):
         return True
     if maxy <= old_maxy and maxy >= old_miny:
         return True
-    
+
+
+def get_ca_conc(my_file, trial, output=output):
+    try:
+        data = ut.get_populations(my_file, trial=trial,
+                                  output=output)
+    except KeyError:
+        return
+                
+    specie_idx = ut.get_all_species(my_file,
+                                    output=output).index("Ca")
+
+    volume = np.array([g[12] for g in grid_list])
+    conc =  ut.nano_molarity(data[:, :, specie_idx],
+                                         volume)
+    new_conc = np.reshape(conc, (data.shape[0],
+                                 102,
+                                 len(volume)//102)).mean(axis=2).T
+              
+    return new_conc
+
+
+def find_clusters(idx_array):
+    clusters = [[[idx_array[0][0], idx_array[1][0]],
+                 [idx_array[0][0], idx_array[1][0]]]]
+    indices = zip(idx_array[0][1:], idx_array[1][1:])
+    for i in range(len(idx_array[0][1:])):
+        new_x, new_y = idx_array[0][i+1], idx_array[1][i+1]
+        for  j, c in enumerate(clusters):
+            if c[0][0]-10 <= new_x <= c[1][0]+10:
+                if c[0][1]-4 <= new_y <= c[1][1]+4:
+                    new_minx = min([c[0][0], c[1][0], new_x])
+                    new_maxx = max([c[0][0], c[1][0], new_x])
+                    new_miny = min([c[0][1], c[1][1], new_y])
+                    new_maxy = max([c[0][1], c[1][1], new_y])
+                    clusters[j] = [[new_minx, new_miny],
+                                   [new_maxx, new_maxy]]
+                    break
+        else:
+            clusters.append([[new_x, new_y], [new_x, new_y]])
+    return clusters
+
+
+def purge_clusters(clusters):
+    new_clusters = []
+    for i, c in enumerate(clusters):
+        new_p1, new_p2 = c
+        for j, nc in enumerate(new_clusters):
+            p1, p2 = nc
+            if (p1[0] <= new_p1[0] <= p2[0]
+                or p1[0] <= new_p2[0] <= p2[0]):
+                if (p1[1] <= new_p1[1] <= p2[1]
+                    or p1[1]<= new_p2[1] <=p2[1]):
+                    c_minx = min([p1[0], p2[0],
+                                  new_p1[0], new_p2[0]])
+                    c_maxx = max([p1[0], p2[0],
+                                  new_p1[0], new_p2[0]])
+                    c_miny = min([p1[1], p2[1],
+                                  new_p1[1], new_p2[1]])
+                    c_maxy = max([p1[1], p2[1],
+                                  new_p1[1], new_p2[1]])
+                    new_clusters[j] = [[c_minx, c_miny],
+                                       [c_maxx, c_maxy]]
+                    break
+        else:   
+            if  c[1][1] - c[0][1]>=5 and c[1][0] - c[0][0]>=5:
+                new_clusters.append(c)
+    if len(new_clusters):
+        new_clusters = sorted(new_clusters, key=lambda x:x[0][0])
+    return new_clusters
+
+
+def get_width(p01, p02):
+    return (p02[1] - p01[1])*0.2
+
+
+def get_len(p01, p02):
+    return (p02[0]-p01[0])/2+0.5
+
+
+def max_amp(conc, p01, p02):
+    return conc[p01[0]:p02[0]+1, p01[1]:p02[1]+1].max()
+
+
+def peak_init(conc, p01):
+    return conc[:, p01[1]].argmax()/2
+
+
+def get_ipi(clusters):
+    peak_dist = []
+    for j, nc in enumerate(new_clusters):
+        p1, p2 = nc
+        if j:
+            o_p1, o_p2 = new_clusters[j-1]
+            new_t = (p2[1]+p1[1])/2*0.2
+            old_t = (o_p2[1]+o_p1[1])/2*0.2
+            peak_dist.append(abs(new_t - old_t))
+    return peak_dist
+
 
 if __name__ == "__main__":
     data_dir = os.path.join("..", "stacked_ER")
     x_labels = list(names_dict.keys())
-    # fig_m_ca, ax_m_ca = plt.subplots(1, len(dend_diam),
-    #                                   figsize=(len(dend_diam)*5, 5))
-    # fig_peak_no, ax_peak_no = plt.subplots(1, len(dend_diam),
-    #                                        figsize=(len(dend_diam)*5, 5))
-    # fig_m_peak_amp, ax_m_peak_amp = plt.subplots(1, len(dend_diam),
-    #                                              figsize=(len(dend_diam)*5, 5))
-    # fig_m_peak_width, ax_m_peak_width = plt.subplots(1, len(dend_diam),
-    #                                                  figsize=(len(dend_diam)*5,
-    #                                                           5)) 
-    # fig_m_peak_dist, ax_m_peak_dist = plt.subplots(1, len(dend_diam),
-    #                                                figsize=(len(dend_diam)*5,
-    #                                                         5))
+    fig_peak_no, ax_peak_no = plt.subplots(1, 1,
+                                           figsize=(1*5, 5))
+    fig_m_peak_amp, ax_m_peak_amp = plt.subplots(1, 1,
+                                                 figsize=(1*5,
+                                                          5))
+    fig_m_peak_width, ax_m_peak_width = plt.subplots(1, 1,
+                                                     figsize=(1*5,
+                                                              5))
+    fig_m_peak_len, ax_m_peak_len = plt.subplots(1, 1,
+                                                 figsize=(1*5,
+                                                              5)) 
+    fig_m_peak_dist, ax_m_peak_dist = plt.subplots(1, 1,
+                                                   figsize=(1*5,
+                                                            5))
+    fig_m_peak_init, ax_m_peak_init = plt.subplots(1, len(dend_diam),
+                                                   figsize=(len(dend_diam)*5,
+                                                            5))
    
     
     for i, d in enumerate(dend_diam):
@@ -71,9 +166,12 @@ if __name__ == "__main__":
         amp_std = []
         width_mean = []
         width_std = []
+        len_mean = []
+        len_std = []
         dist_mean = []
         dist_std = []
-        for fname in names_dict.values():
+        xlabels = []
+        for key, fname in names_dict.items():
             path = os.path.join(data_dir, fname % d)
             my_file = h5py.File(path)
             grid_list = ut.get_grid_list(my_file)
@@ -84,106 +182,110 @@ if __name__ == "__main__":
             peak_dist = []
             peak_start = []
             for trial in ["trial0", "trial1", "trial2", "trial3", "trial4"]:
-                try:
-                    data = ut.get_populations(my_file, trial=trial,
-                                              output=output)
-                except KeyError:
+                new_conc = get_ca_conc(my_file, trial)
+                if new_conc is None:
                     continue
-                specie_idx = ut.get_all_species(my_file,
-                                                output=output).index("Ca")
-
-                volume = np.array([g[12] for g in grid_list])
-                conc =  ut.nano_molarity(data[:, :, specie_idx],
-                                         volume)
-                new_conc = np.reshape(conc, (data.shape[0],
-                                             102,
-                                             len(volume)//102)).mean(axis=2).T
-              
-                fig, ax = plt.subplots(1, 3, figsize=(15, 5))
                 where = np.where(new_conc > 2.5*76)
                 if not len(where[0]):
                     continue
-                edges = np.zeros_like(new_conc)
-                for x, y in zip(where[0], where[1]):
-                   edges[x, y] = 1
 
-                
+                my_clusters = find_clusters(where)
+                new_clusters = purge_clusters(my_clusters)
+                peak_no.append(len(new_clusters))
+                peak_dist.extend(get_ipi(new_clusters))
+                                 
+                for j, nc in enumerate(new_clusters):
+                    p1, p2 = nc
+                    peak_width.append(get_width(p1, p2))
+                    peak_len.append(get_len(p1, p2))
+                    peak_amp.append(new_conc[p1[0]:p2[0]+1,
+                                             p1[1]:p2[1]+1].max())
+                    peak_start.append(new_conc[:, p1[1]].argmax()/2)
 
-           
-                clusters = [[[where[0][0], where[1][0]],
-                             [where[0][0], where[1][0]]]]
-                indices = zip(where[0][1:], where[1][1:])
-                for i in range(len(where[0][1:])):
-                    new_x, new_y = where[0][i+1], where[1][i+1]
-                    for  j, c in enumerate(clusters):
-                        if c[0][0]-10 <= new_x <= c[1][0]+10:
-                            if c[0][1]-4 <= new_y <= c[1][1]+4:
-                                new_minx = min([c[0][0], c[1][0], new_x])
-                                new_maxx = max([c[0][0], c[1][0], new_x])
-                                new_miny = min([c[0][1], c[1][1], new_y])
-                                new_maxy = max([c[0][1], c[1][1], new_y])
-                                clusters[j] = [[new_minx, new_miny],
-                                               [new_maxx, new_maxy]]
-                                break
-                    else:
-                        clusters.append([[new_x, new_y], [new_x, new_y]])
-
-                new_clusters = []
-             
-                for i, c in enumerate(clusters):
-                    new_p1, new_p2 = c
-                    for j, nc in enumerate(new_clusters):
-                        p1, p2 = nc
-                        if (p1[0] <= new_p1[0] <= p2[0]
-                            or p1[0] <= new_p2[0] <= p2[0]):
-                            if (p1[1] <= new_p1[1] <= p2[1]
-                                or p1[1]<= new_p2[1] <=p2[1]):
-                                c_minx = min([p1[0], p2[0],
-                                              new_p1[0], new_p2[0]])
-                                c_maxx = max([p1[0], p2[0],
-                                              new_p1[0], new_p2[0]])
-                                c_miny = min([p1[1], p2[1],
-                                              new_p1[1], new_p2[1]])
-                                c_maxy = max([p1[1], p2[1],
-                                              new_p1[1], new_p2[1]])
-                                new_clusters[j] = [[c_minx, c_miny],
-                                                   [c_maxx, c_maxy]]
-                                break
-                    else:   
-                        if  c[1][1] - c[0][1]>=5 and c[1][0] - c[0][0]>=5:
-                            new_clusters.append(c)
-                if len(new_clusters):
-
-                    new_clusters = sorted(new_clusters, key=lambda x:x[0][0])
-                    peak_no.append(len(new_clusters))
                     
-                    for j, nc in enumerate(new_clusters):
-                        p1, p2 = nc
-                        peak_width.append((p2[1] - p1[1])*0.2)
-                        peak_len.append((p2[0]-p1[0])/2+0.5)
-                        peak_amp.append(new_conc[p1[0]:p2[0]+1,
-                                             p1[1]:p2[1]+1].mean())
-                        
-                        peak_start.append(new_conc[:, p1[1]].argmax()/2)
-                        if j:
-                            o_p1, o_p2 = new_clusters[j-1]
-                            new_t = (p2[1]+p1[1])/2*0.2
-                            old_t = (o_p2[1]+o_p1[1])/2*0.2
-                            peak_dist.append(abs(new_t - old_t))
-       
-        print(fname)
-        peak_no = np.array(peak_no)
-        peak_amp = np.array(peak_amp)
-        peak_width = np.array(peak_width)
-        peak_len = np.array(peak_len)
-        peak_dist = np.array(peak_dist)
-        peak_start = np.array(peak_start)
-        print(peak_no, peak_no.mean(), peak_no.std()/len(peak_no)**.5)
-        print(peak_len, peak_len.mean(), peak_len.std()/len(peak_len)**.5)
-        print(peak_width, peak_width.mean(), peak_width.std()/len(peak_width)**.5)
-        print(peak_dist, peak_dist.mean(), peak_dist.std()/len(peak_dist)**.5)
-        print(peak_start, peak_start.mean(), peak_start.std()/len(peak_start)**.5)
+            print(path)
+            xlabels.append(key)
+            print(peak_start)
+            peak_no = np.array(peak_no)
+            peak_amp = np.array(peak_amp)
+            peak_width = np.array(peak_width)
+            peak_len = np.array(peak_len)
+            peak_dist = np.array(peak_dist)
+            
+            xlabels_for_peak_start = [key]*len(peak_start)
+            no_mean.append(peak_no.mean())
+            no_std.append(peak_no.std()/len(peak_no)**.5)
+            amp_mean.append(peak_amp.mean())
+            amp_std.append(peak_amp.std()/len(peak_amp)**.5)
+            width_mean.append(peak_width.mean())
+            width_std.append( peak_width.std()/len(peak_width)**.5)
+            len_mean.append(peak_len.mean())
+            len_std.append( peak_len.std()/len(peak_len)**.5)
+            dist_mean.append(peak_dist.mean())
+            dist_std.append(peak_dist.std()/len(peak_dist)**.5)
+            
+            if len(peak_start):
+                ax_m_peak_init[i].plot(peak_start, xlabels_for_peak_start,
+                                       color=colors[d],
+                                       marker="o", linestyle="")
+        ax_peak_no.errorbar(y=xlabels, x=no_mean,
+                            xerr=no_std,
+                            color=colors[d],
+                            marker="o", linestyle="",
+                            label="%s um diam" % d)
 
-        print(peak_amp, peak_amp.mean(), peak_amp.std()/len(peak_amp)**.5)
+        ax_m_peak_amp.errorbar(y=xlabels, x=amp_mean,
+                               xerr=amp_std,
+                               color=colors[d],
+                               marker="o", linestyle="",
+                               label="%s um diam" % d)
+        ax_m_peak_width.errorbar(y=xlabels, x=width_mean,
+                                 xerr=width_std,
+                                 color=colors[d],
+                                 marker="o", linestyle="",
+                                 label="%s um diam" % d)
+        ax_m_peak_len.errorbar(y=xlabels, x=len_mean,
+                               xerr=len_std,
+                               color=colors[d],
+                               marker="o", linestyle="",
+                               label="%s um diam" % d)
+        ax_m_peak_dist.errorbar(y=xlabels, x=dist_mean,
+                                xerr=dist_std,
+                                color=colors[d],
+                                marker="o", linestyle="",
+                                label="%s um diam" % d)
+                                
+        if i:
+            ax_m_peak_init[i].set_yticklabels([])
+
+        ax_m_peak_init[i].set_title("diam %s um" % d)
+        ax_m_peak_init[i].set_xlabel("initiation on dendrite [um]")
+
+    ax_peak_no.set_xlabel("# calcium peaks")
+    ax_m_peak_amp.set_xlabel("mean Ca peak amplitude (nM)")
+    ax_m_peak_width.set_xlabel("mean Ca peak width (s)")
+    ax_m_peak_len.set_xlabel("mean Ca peak len (s)")
+    ax_m_peak_dist.set_xlabel("mean interval between peaks (s)")
         
-        
+    ax_peak_no.set_yticklabels(xlabels)
+    ax_m_peak_amp.set_yticklabels(xlabels)
+    ax_m_peak_width.set_yticklabels(xlabels)
+    ax_m_peak_len.set_yticklabels(xlabels)
+    ax_m_peak_dist.set_yticklabels(xlabels)
+    ax_peak_no.legend()
+    ax_m_peak_amp.legend()
+    ax_m_peak_width.legend()
+    ax_m_peak_len.legend()
+    ax_m_peak_dist.legend()
+    
+
+    fig_peak_no.savefig("mean_peak_no.png", dpi=100,
+                 bbox_inches="tight")
+    fig_m_peak_amp.savefig("mean_peak_amp.png", dpi=100,
+                 bbox_inches="tight")
+    fig_m_peak_dist.savefig("mean_peak_dist.png", dpi=100,
+                            bbox_inches="tight")
+    fig_m_peak_width.savefig("mean_peak_width.png", dpi=100,
+                            bbox_inches="tight")
+    fig_m_peak_init.savefig("peak_init.png", dpi=100,
+                            bbox_inches="tight")
