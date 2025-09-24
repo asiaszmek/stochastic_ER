07916b050c23a776178513633d174fb4bfecc1f1
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2025-09-24 09:49:06 +0200

    Rename fig scripts to match the manuscript

--- scripts/run_fig_11.py
+++ scripts/run_fig_11.py
@@ -1,54 +1,67 @@
 import os
+import h5py
 import utility_functions as utils
+import matplotlib.pyplot as plt
 
-
-colors = {"1.2": 'tab:blue',
-          "2.4": 'tab:purple',
-          "6.0": 'tab:green'}
-dir_list = [
-    ["Ca_wave_RyR2CaM_simple_SERCA_SOCE_%s",
-    "Ca_wave_RyR2CaM_0.8_PMCA_%s"],
-    ["Ca_wave_RyR2CaM_simple_SERCA_SOCE_%s",
-    "Ca_wave_RyR2CaM_simple_SERCA_SOCE_largerER_%s"],
-    ["Ca_wave_RyR2CaM_simple_SERCA_SOCE_%s",
-    "Ca_wave_RyR2CaM_120_PMCA_%s"],
-    ["Ca_wave_RyR2CaM_simple_SERCA_SOCE_%s",
-    "Ca_wave_2xRyR2CaM_simple_SERCA_SOCE_%s"],
-]    
-directories = {
-    "Ca_wave_RyR2CaM_simple_SERCA_SOCE_%s":
-    "model_RyR2CaM_simple_SERCA_SOCE_tubes_diam_%s_um_50_um_%s_nM.h5", 
-    "Ca_wave_RyR2CaM_0.8_PMCA_%s":
-    "model_RyR2CaM_simple_SERCA_SOCE_0.8_PMCA_tubes_diam_%s_um_50_um_%s_nM.h5" ,
-    "Ca_wave_RyR2CaM_simple_SERCA_SOCE_largerER_%s":
-    "model_RyR2CaM_largerER_simple_SERCA_SOCE_tubes_diam_%s_um_50_um_%s_nM.h5",
-    "Ca_wave_RyR2CaM_120_PMCA_%s":
-    "model_RyR2CaM_simple_SERCA_SOCE_120_PMCA_tubes_diam_%s_um_50_um_%s_nM.h5",
-    "Ca_wave_2xRyR2CaM_simple_SERCA_SOCE_%s":
-    "model_2xRyR2CaM_simple_SERCA_SOCE_tubes_diam_%s_um_50_um_%s_nM.h5", 
-}
-
-types = [r"80\% PMCA activity",
-         r"110\%ER",
-         r"120\% PMCA",
-         r"200\% RyR2",
-         ]
-
-
-stims = ["0175", "0350", "0700", "1050", "2000"]
-dend_diam = ["1.2", "2.4", "6.0"]
+plt.rcParams['text.usetex'] = True
 
 
 if __name__ == '__main__':
+    specie_list = ["Ca"]
+    specie = "Ca"
     base = "dend"
-    reg_list = [base, "dend01", "dend02", "dend0", "dend04",
-                "dend05", "dend06", "dend07", "dend08", "dend09",]
+    reg_list = [base, "dend01", "dend02", "dend03", "dend04", "dend05",
+                "dend06", "dend07", "dend08", "dend09",]
     for i in range(10, 102, 1):
         reg_list.append("%s%d" %(base, i))
-    output_name = "all"        
-    fig1 = utils.make_distance_fig_ratio_bars(dir_list, directories, dend_diam,
-                                              stims, ["Ca"], reg_list, output_name,
-                                              colors, types)
-    fig1.savefig("Param_var_distance_tubes.png", dpi=100, bbox_inches="tight")
-    fig1.savefig("Param_var_distance_tubes.eps", dpi=100, bbox_inches="tight")
-
+    fnames = [
+        os.path.join("..",
+                     "model_RyR_RyRCaM_0.8_PMCA",
+                     "model_RyR_RyRCaM_0.8_PMCA_simple_SERCA_tubes_diam_2.4_um_2_um_dendrite.h5"),
+        os.path.join("..",
+                     "model_RyR_0.8_PMCA",
+                     "model_RyR_simple_SERCA_0.8_PMCA_tubes_diam_2.4_um_10_um_dendrite.h5")
+    ]
+    figs, axes = [], []
+    for fname in fnames:
+        my_file = h5py.File(fname, 'r')
+        new_fname = os.path.split(fname)[-1]
+        conc_dict = {}
+        time_dict = {}
+        for trial in my_file.keys():
+            if trial == "model":
+                continue
+            conc, voxels = utils.get_dynamics_in_region(my_file,
+                                                        specie_list,
+                                                        reg_list, trial, "__main__")
+            conc_dict[trial] = conc
+            time = utils.get_times(my_file, trial, "__main__")
+            time_dict[trial] = time
+        vmin = 0
+        vmax = 1200
+        diam = fname.split("diam_")[-1][:3]
+        for key in conc_dict:
+            fig, ax = plt.subplots(1, 1)
+            time = time_dict[key]
+            im = ax.imshow(conc_dict[key].T, aspect="auto",
+                           interpolation="none",
+                           origin="lower", extent = [time[0]*1e-3,
+                                                     time[-1]*1e-3,
+                                                     voxels[0],
+                                                     voxels[-1]],
+                           cmap=plt.get_cmap("Reds"))
+            ax.set_xlabel(r"time (s)", fontsize=15)
+            ax.set_ylabel(r"dendrite $(\mathrm{\mu m})$", fontsize=15)
+            fig.colorbar(im)
+            
+            ax.set_title(r"%s dynamics in %s $\mathrm{\mu m}$ dend" % (specie, diam),
+                         fontsize=14)
+            fig.savefig(new_fname[:-3]+"_"+key+".png", dpi=100,
+                        bbox_inches="tight")
+            fig.savefig(new_fname[:-3]+"_"+key+".eps", dpi=100,
+                        bbox_inches="tight")
+    
+    
+    
+ 
+                          
---
+++ scripts/run_fig_9_2.py
@@ -0,0 +1,54 @@
+import os
+import utility_functions as utils
+from matplotlib.lines import Line2D
+
+colors ={
+
+        "1.2": 'tab:blue',
+        "2.4": 'tab:purple',
+        "6.0": 'tab:green'
+
+}
+directories = [
+    ["Ca_wave_RyR2CaM_simple_SERCA_SOCE_%s",
+     "model_RyR2CaM%s_simple_SERCA_SOCE_tubes_diam_%s_um_50_um_%s_nM.h5"],
+    ["Ca_wave_simple_SERCA_no_SOCE_%s",
+     "model_RyR%s_simple_SERCA_tubes_diam_%s_um_50_um_%s_nM.h5"],
+    ["Ca_wave_RyR2CaM_simple_SERCA_SOCE_2x_buffers_%s",
+     "model_RyR2CaM%s_simple_SERCA_SOCE_2x_buffer_tubes_diam_%s_um_50_um_%s_nM.h5"],
+    ["Ca_wave_normal_SERCA_aging_%s", "model_aging%s_simple_SERCA_tubes_diam_%s_um_50_um_%s_nM.h5"],
+]
+legend_elements = [Line2D([0], [0], color='k', marker="o", fillstyle="full",
+                          lw=0, label='ctrl'),
+                   Line2D([0], [0], color="k", marker='o', fillstyle="none",
+                          lw=0, label='ctrl with old age RyR2'),
+
+                   Line2D([0], [0], color="k", marker='^', fillstyle="none",
+                          lw=0, label='old age ctrl RyR2'),
+                   Line2D([0], [0], color='k', marker="s", fillstyle="none",
+                          lw=0, label='old age'),]
+
+
+stims = ["0175", "0350", "0700", "1050", "2000"]
+dend_diam = ["1.2"]#, "2.4", "6.0"]
+
+if __name__ == '__main__':
+    types = [ "ctrl",  "ctrl with old age RyR2", "ctrl + 200\% Ca buffers",
+              "old age",]
+    marker = ["o", "o", "^","s", ]
+    fillstyle = ["full", "none",  "none", "none"]
+
+    output_name = "all"
+    fig1 = utils.make_decay_constant_fig_sep_dends(directories,
+                                                   dend_diam,
+                                                   stims,
+                                                   output_name, 
+                                                   colors,
+                                                   types, marker,
+                                                   fillstyle)
+    fig1.savefig("Aging_temporal_dissection.png", dpi=100,
+                 bbox_inches="tight")
+    fig1.savefig("Aging_temporal_dissection.eps", dpi=100,
+                 bbox_inches="tight")
+
+
